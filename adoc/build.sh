88dfd9d806ec8ee1e5d53a46366c076bb5885773
vittali <peter.vittali@openfokus.org>
2024-01-26 18:30:07 +0100

    v2

--- adoc/build.sh
+++ adoc/build.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 asciidoctor -D ../build **/*.adoc
 rsync -a --verbose --perms --times  --prune-empty-dirs --delete-after images ../build
+#linkcheck -e --no-show-redirects http://localhost:8080/TiPs_6/index.html
